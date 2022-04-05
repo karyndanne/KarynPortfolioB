aedd2e3c2b8142d399dff93cc0d169c175a76525
Karyndanne <95543376+karyndanne@users.noreply.github.com>
2022-04-05 00:17:46 -0400

    UI additions

---
+++ node.js
@@ -0,0 +1,7 @@
+$('#myTab a').on('shown.bs.tab', function () {
+    // do something...
+  })
+
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
